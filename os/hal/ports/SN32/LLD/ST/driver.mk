df4f1b7433639120c490d2af574d5f215d20ab47
IsaacDynamo <61521674+IsaacDynamo@users.noreply.github.com>
2021-08-30 18:01:05 +0200

    Add generic SN32 SysTick driver

---
+++ os/hal/ports/SN32/LLD/ST/driver.mk
@@ -0,0 +1,10 @@
+ifeq ($(USE_SMART_BUILD),yes)
+ifneq ($(findstring HAL_USE_CT TRUE,$(HALCONF)),)
+PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/LLD/ST/hal_st_lld.c
+endif
+else
+PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/LLD/ST/hal_st_lld.c
+endif
+
+
+PLATFORMINC += $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/LLD/ST
